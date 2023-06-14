f89402f80529cf1fcb44679d61d28fd5b10f4e09
Andre Amboni <amboni.andre@gmail.com>
2023-06-14 20:39:49 -0300

    Adicionando getter e setter para o id de Paises

--- src/main/java/com/books/model/Pais.java
+++ src/main/java/com/books/model/Pais.java
@@ -16,6 +16,14 @@ public class Pais {
 
     @Column(nullable = false)
     private String nome;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
